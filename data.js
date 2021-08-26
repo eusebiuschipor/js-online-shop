08d821375d9309e29342b9cb5ee0156fec9be875
eusebiuschipor <eusebiu@eusebiuschipor.com>
2021-08-26 18:00:56 +0300

    First commit

---
+++ data.js
@@ -0,0 +1,34 @@
+const availableProducts = [
+    {
+        id: 1,
+        title: 'iPhone X',
+        price: 100
+    },
+    {
+        id: 2,
+        title: 'iPhone 11',
+        price: 200
+    },
+    {
+        id: 3,
+        title: 'iPhone 12',
+        price: 300
+    },
+    {
+        id: 4,
+        title: 'Google Pixel',
+        price: 400
+    },
+    {
+        id: 5,
+        title: 'Google Pixel 2',
+        price: 500
+    },
+    {
+        id: 6,
+        title: 'Google Pixel 3',
+        price: 600
+    }
+];
+
+export { availableProducts };
